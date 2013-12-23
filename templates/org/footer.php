5d895fb55e95d81fe877d7197446855229fdaf62
Sean Girard <seangirard@yahoo.com>
2013-12-23 11:20:53 -0800

    footer

--- templates/org/footer.php
+++ templates/org/footer.php
@@ -7,42 +7,21 @@
   <footer class="content-info container" role="contentinfo">
 
     <div class="row">
-      <div class="col-md-4">
+      <div class="col-md-12">
+        <span class="pull-right">
+          <img alt="City of Spokane logo" src="http://www.spokanelibrary.org/new/img/city-spokane-logo.png">
+        </span>
+
         <p>
-          <span class="text-muted serif">
-            <?php bloginfo('name'); ?>
-          </span>
+          <strong class="text-muted" style="">Spokane Public Library</strong>
         </p>
-        <address class="text-muted serif">
-          <small>
+        <address class="text-muted" style="">
           906 West Main Avenue
           <br>
           Spokane, WA 99201
           <br>
-          509.444.5318
-          </small>
+          509.444.5300
         </address>
-      </div><!-- /.col -->
-
-      <div class="col-md-4">
-        <p class="text-center">
-          <small class="glyphicon glyphicon-envelope text-primary"></small>
-          <a href="mailto:mail@spokanelibraryfdtn.org">mail@spokanelibraryfdtn.org</a>
-        </p>
-        <p class="text-center">
-          <a class="text-muted" href="/financial-information"><small>FEIN: 91-1917727</small></a>
-        </p>
-      </div><!-- /.col -->
-
-      <div class="col-md-4">
-        <p class="text-right">
-          <span class="text-muted serif" >
-          <?php bloginfo('description'); ?>
-         </span>
-        </p>
-        <p class="text-right">
-          <a href="http://www.spokanelibrary.org"><img alt="Spokane Public Library Logo" src="/assets/img/SPL-Logo-beta.png"></a>
-        </p>
       </div><!-- /.col -->
     </div><!-- /.row -->
 
