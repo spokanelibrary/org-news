52b10fc30f39a5d6ac329e99d525efee184efc51
Sean Girard <seangirard@yahoo.com>
2013-12-23 13:19:53 -0800

    masthead

--- templates/org/header-top-navbar.php
+++ templates/org/header-top-navbar.php
@@ -48,9 +48,9 @@
     </div>
     <div class="serif" style="padding-top:10px;">
       <em class="text-primary" style="color:#fff;">
-      <span style="font-size:30px; line-height:35px; margin:0; padding:0; padding-left:30px;">News</span>
+      <span style="font-size:40px; line-height:40px; margin:0; padding:0; padding-left:30px;">News</span>
       <br>
-      <span style="font-size:20px; line-height:25px; margin:0; padding:0;  padding-left:30px; margin:0;">from your library</span>
+      <span style="font-size:16px; line-height:16px; margin:0; padding:0;  padding-left:30px; margin:0;">from your library</span>
       </em>
     </div>
   </div>
