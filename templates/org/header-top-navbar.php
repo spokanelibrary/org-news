518c243af929f725e12ef96fa87111a100ea4309
Sean Girard <seangirard@yahoo.com>
2013-12-23 13:15:43 -0800

    masthead

--- templates/org/header-top-navbar.php
+++ templates/org/header-top-navbar.php
@@ -48,7 +48,7 @@
     </div>
     <div class="serif" style="">
       <em class="text-primary">
-      <span style="font-size:30px; padding-left:30px; margin:0;">News &amp; Ideas</span>
+      <span style="font-size:30px; padding-left:30px; line-height:30px;">News &amp; Ideas</span>
       <br>
       <span style="font-size:20px; padding-left:30px; margin:0;">from your library</span>
       </em>
