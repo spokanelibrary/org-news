e03689016164f99dceee9a34d7f31f682a0a91a9
Sean Girard <seangirard@yahoo.com>
2014-01-08 14:36:14 -0800

    home

--- template-home.php
+++ template-home.php
@@ -14,8 +14,10 @@ Template Name: - Home Page
     </p>
 
     <?php echo do_shortcode('[spl_mailgun_current format=toc]'); ?>
-    <?php echo do_shortcode('[spl_mailgun_current title="Read all about it &rarr;" class="btn btn-block btn-primary"]'); ?>
 
+    <p>
+    <?php echo do_shortcode('[spl_mailgun_current title="Read all about it &rarr;" class="btn btn-block btn-primary"]'); ?>
+    </p>
   </div>
 </div>
 <?php get_template_part('templates/content', 'page'); ?>
