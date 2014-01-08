ee1d11c662dc578c9008f06e7fce3e25d6a269ce
Sean Girard <seangirard@yahoo.com>
2014-01-08 13:02:05 -0800

    home

--- template-home.php
+++ template-home.php
@@ -4,23 +4,12 @@ Template Name: - Home Page
 */
 ?>
 
-<p>
-  <img class="img-responsive" src="/assets/img/jpg/promo-foundation.jpg" >
-</p>
-
 <?php get_template_part('templates/page', 'header'); ?>
 
-<div class="row">
-
-  <div class="col-sm-6">
-    <?php get_template_part('templates/content', 'page'); ?>
-  </div><!-- /.col -->
-  
-  <div class="col-sm-6">
-    <div class="alert alert-success">
-      <?php echo do_shortcode('[spl_mailgun_current]'); ?>
-    </div>
-  </div><!-- /.col -->
-
-</div><!-- /.row -->
+<div class="pull-right">
+  <div class="alert alert-success">
+    <?php echo do_shortcode('[spl_mailgun_current]'); ?>
+  </div>
+</div>
+<?php get_template_part('templates/content', 'page'); ?>
 
