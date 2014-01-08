bcfac489164d7baf4c60387cdabb79ea169b33b9
Sean Girard <seangirard@yahoo.com>
2014-01-08 10:46:26 -0800

    archive

--- archive-newsletter.php
+++ archive-newsletter.php
@@ -14,7 +14,8 @@ This is some intro material
     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
   </header>
   <div class="entry-summary">
-    <time class="published" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time> | <?php the_excerpt(); ?>
+    <time class="published pull-left" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
+    <?php the_excerpt(); ?>
   </div>
 </article>
 <?php endwhile; ?>
