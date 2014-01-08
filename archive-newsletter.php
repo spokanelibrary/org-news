df81f3848024173db29b425c945c80b128accbf7
Sean Girard <seangirard@yahoo.com>
2014-01-08 10:48:25 -0800

    archive

--- archive-newsletter.php
+++ archive-newsletter.php
@@ -11,11 +11,15 @@ This is some intro material
 <?php while (have_posts()) : the_post(); ?>
 <article <?php post_class(); ?>>
   <header>
-    <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+    <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+    	<small>
+    		<time class="published" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
+    	</small>
+    </h2>
   </header>
   <div class="entry-summary">
   	<div class="alert alert-success pull-left">
-    <time class="published" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
+    
     </div>
     <?php the_excerpt(); ?>
   </div>
