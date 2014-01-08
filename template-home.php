df00e332ec74b08e9ef0b19b1ce4cd8b5fbcfb62
Sean Girard <seangirard@yahoo.com>
2014-01-08 13:06:18 -0800

    home

--- template-home.php
+++ template-home.php
@@ -9,9 +9,12 @@ Template Name: - Home Page
 <div class="pull-left" style="padding-right: 20px;">
   <div class="alert alert-success">
     <p class="lead">
-      In the current edition
+      In the current edition:
     </p>
     <?php echo do_shortcode('[spl_mailgun_current]'); ?>
+    <p>
+      Also in this edition&hellip;
+    </p>
   </div>
 </div>
 <?php get_template_part('templates/content', 'page'); ?>
