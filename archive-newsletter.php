672e8f19134329ee875543d9a9a0d1e428e0182c
Sean Girard <seangirard@yahoo.com>
2014-01-08 10:35:23 -0800

    archive

--- archive-newsletter.php
+++ archive-newsletter.php
@@ -1,1 +1,6 @@
 These are the newsletter archives.
+
+<?php while (have_posts()) : the_post(); ?>
+  <?php the_content(); ?>
+  <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
+<?php endwhile; ?>
