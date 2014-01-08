ea7c7b233d295e7b14bef465335cbeabfd756ca6
Sean Girard <seangirard@yahoo.com>
2014-01-08 13:09:17 -0800

    home

--- template-home.php
+++ template-home.php
@@ -6,12 +6,14 @@ Template Name: - Home Page
 
 <?php get_template_part('templates/page', 'header'); ?>
 
-<div class="pull-left" style="padding-right: 20px;">
+<div class="pull-left" style="padding: 0 20px 20px 0;">
   <div class="alert alert-success">
     <p class="lead">
       In the current edition:
     </p>
+    <p>
     <?php echo do_shortcode('[spl_mailgun_current class="btn btn-primary btn-block"]'); ?>
+    </p>
     <p>
       Also in this edition&hellip;
     </p>
