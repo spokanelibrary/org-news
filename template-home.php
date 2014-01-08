9b584cfa9133f1aefc87c550ad5348e2b54611df
Sean Girard <seangirard@yahoo.com>
2014-01-08 12:56:44 -0800

    home

--- template-home.php
+++ template-home.php
@@ -4,61 +4,20 @@ Template Name: - Home Page
 */
 ?>
 
-<?php //get_template_part('templates/page', 'header'); ?>
-
-<p>
-  <img class="img-responsive" src="/assets/img/jpg/promo-foundation.jpg" >
-<!--<img class="img-responsive" src="http://lorempixel.com/1200/300/people" > -->
-</p>
-
 <?php get_template_part('templates/page', 'header'); ?>
-<?php get_template_part('templates/content', 'page'); ?>
 
+<div class="row">
 
-<footer class="content-info container" role="contentinfo">
-  <div class="row">
+  <div class="col-sm-6">
+    <?php get_template_part('templates/content', 'page'); ?>
+  </div><!-- /.col -->
+  
+  <div class="col-sm-6">
 
-    <div class="col-sm-6">
-      <div class="panel panel-success panel-callout">
-        <p class="lead text-success">
-          About the Foundation
-        </p>
-
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-          Cras nec rutrum nisi. 
-          Vivamus eget suscipit purus, ut accumsan augue.
-        </p>
-
-        <p class="text-right">
-          <a class="btn btn-primary" href="/mission-vision">
-            <i class="glyphicon glyphicon-book"></i> Learn More &rarr;
-          </a>
-        </p>
-      </div>
-      
-    </div><!-- /.col -->
+    <div class="alert alert-success">
+      <?php do_shortcode('spl_mailgun_current'); ?>
+    </div>
     
-    <div class="col-sm-6">
-      <div class="panel panel-success panel-callout">
-        <p class="lead text-success">
-          Make a difference
-        </p>
+  </div><!-- /.col -->
 
-        <p>
-          Nunc eu consectetur odio, sit amet aliquet dolor. 
-          Vestibulum dictum eros neque, non posuere lacus iaculis at.
-          Etiam fringilla odio nec tempor molestie.
-        </p>
-
-        <p class="text-right">
-          <a class="btn btn-success" href="/donate">
-            <i class="glyphicon glyphicon-gift"></i> Donate Now &rarr;
-          </a>
-        </p>
-      </div>
-      
-    </div><!-- /.col -->
-
-  </div><!-- /.row -->
-</footer>
+</div><!-- /.row -->
