e58a3b5c5a9d325ba3e34f76a1229247df655b5a
Sean Girard <seangirard@yahoo.com>
2014-01-09 09:07:53 -0800

    home

--- template-home.php
+++ template-home.php
@@ -6,22 +6,31 @@ Template Name: - Home Page
 
 <?php get_template_part('templates/page', 'header'); ?>
 
-<div class="pull-left" style="padding: 20px 20px 0 0;">
-  <div class="alert alert-success">
+<div clas="row">
+  <div class="col-md-4">
+    <div class="alert alert-success">
 
-    <p class="lead">
-      In the current edition:
-    </p>
+      <p class="lead">
+        In the current edition:
+      </p>
 
-    <?php echo do_shortcode('[spl_mailgun_current format=toc]'); ?>
-    <hr>
-    <?php echo do_shortcode('[spl_mailgun_current title="Read the latest newsletter &rarr;" class="btn btn-block btn-success"]'); ?>
+      <?php echo do_shortcode('[spl_mailgun_current format=toc]'); ?>
+      <hr>
+      <?php echo do_shortcode('[spl_mailgun_current title="Read the latest newsletter &rarr;" class="btn btn-block btn-success"]'); ?>
+    </div>
   </div>
+  <div class="col-md-8">
+    <?php get_template_part('templates/content', 'page'); ?>
+  </div>
+</div>
+
+<div class="pull-left" style="padding: 20px 20px 0 0;">
+  
   <!--
   <p>
     <a href="/subscribe/">Sign me up for Library news &rarr;</a>
   </p>
   -->
 </div>
-<?php get_template_part('templates/content', 'page'); ?>
+
 
