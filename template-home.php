dc23c8c15b3d41a5a55547044561c64310910c9b
Sean Girard <seangirard@yahoo.com>
2014-01-09 10:10:35 -0800

    home

--- template-home.php
+++ template-home.php
@@ -43,9 +43,9 @@ Template Name: - Home Page
     </div>
   </div>
   <div class="col-md-8">
-    <p>
+    <strong>
     Read it here, or have the news delivered to your inbox.
-    </p>
+    </strong>
     <p class="lead text-center">
       <a href="/subscribe/">Sign me up for Library News →</a>
     </p>
