499671c56f488d4eac8a797b6569aeb192a082f9
Sean Girard <seangirard@yahoo.com>
2014-01-09 09:11:36 -0800

    home

--- template-home.php
+++ template-home.php
@@ -6,7 +6,10 @@ Template Name: - Home Page
 
 <?php get_template_part('templates/page', 'header'); ?>
 
-<h3>Library News <small>published monthly, with occasional special editions</small></h3>
+<h3 style="margin-top:0;">
+  Library News 
+  <small>published monthly, with occasional special editions</small>
+</h3>
 
 <div clas="row">
   <div class="col-md-4" style="">
