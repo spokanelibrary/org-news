c8329f65a1e2337755085fd3025d39032a437a44
Sean Girard <seangirard@yahoo.com>
2014-01-09 15:15:14 -0800

    home

--- template-home.php
+++ template-home.php
@@ -30,7 +30,7 @@ Template Name: - Home Page
 
 <div clas="row">
   <div class="col-md-4">
-    <div class="alert alert-warning" style="border-left-width:4px; margin-top:20px;">
+    <div class="alert alert-warning" style="border-left-width:4px; margin-top:10px;">
 
       <p class="lead">
         <small class="glyphicon glyphicon-bullhorn"></small>
