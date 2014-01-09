98a8776e17916719f7dc62c88a10b8e4566c33f9
Sean Girard <seangirard@yahoo.com>
2014-01-09 14:15:07 -0800

    sidebar

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -1,1 +1,14 @@
+<div class="alert alert-warning" style="border-left-width:4px;">
+
+  <p class="lead">
+    <small class="glyphicon glyphicon-bullhorn"></small>
+    In the current edition
+  </p>
+
+  <?php echo do_shortcode('[spl_mailgun_current format=toc]'); ?>
+  <hr>
+  <?php echo do_shortcode('[spl_mailgun_current title="Read the current newsletter &rarr;" class="btn btn-block btn-warning"]'); ?>
+
+</div>
+
 <?php dynamic_sidebar('sidebar-primary'); ?>
