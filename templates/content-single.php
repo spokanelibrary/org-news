82c89aae8b1872ff3b15aed8976d92be50c88e5d
Sean Girard <seangirard@yahoo.com>
2014-01-10 14:25:37 -0800

    meta

--- templates/content-single.php
+++ templates/content-single.php
@@ -8,12 +8,13 @@
       <?php the_content(); ?>
     </div>
 
-    <?php get_template_part('templates/entry-meta'); ?>
     <p class="lead text-center text-muted">
       &hellip;
       <i class="glyphicon glyphicon-leaf" style="padding: 0 8px 0 12px;"></i>
       &hellip;
     </p>
+
+    <?php get_template_part('templates/entry-meta'); ?>
 
     <footer>
       <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
