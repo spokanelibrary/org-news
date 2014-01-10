15e9ca8b5e59bd6daea94f5ddeb2d934ba1447f4
Sean Girard <seangirard@yahoo.com>
2014-01-10 10:20:37 -0800

    responsive headers

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '5fe5e5bc436549e328a774fef25ca9d8');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '839cc53f6a17971e7cfe9e87ad57299b');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
