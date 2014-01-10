79b3182f74921014283fc4e10548c3978fea4cec
Sean Girard <seangirard@yahoo.com>
2014-01-10 07:52:42 -0800

    home

--- template-home.php
+++ template-home.php
@@ -34,7 +34,7 @@ Template Name: - Home Page
     </div>
   </div>
   <div class="col-md-8">
-    <div class="panel panel-primary">
+    <div class="panel panel-success">
       <div class="panel-heading">
         Read it here or have the news delivered to your inbox.
         </div>
