1f0c656befb63ace8fd0c047dc82bbf275b74f58
Sean Girard <seangirard@yahoo.com>
2014-01-10 07:38:42 -0800

    home

--- template-home.php
+++ template-home.php
@@ -15,7 +15,7 @@ Template Name: - Home Page
   </div>
 -->
 <h3 class="text-success" style="margin-top:0;">
-  Published monthly, with occasional special editions
+  <em>Library News:</em> published monthly, with occasional special editions
   <small></small>
 </h3>
 
@@ -37,7 +37,7 @@ Template Name: - Home Page
         In the current edition
       </p>
 
-      <?php echo do_shortcode('[spl_mailgun_current format=toc link_posts]'); ?>
+      <?php echo do_shortcode('[spl_mailgun_current format=toc link_posts=true]'); ?>
       <hr>
       <?php echo do_shortcode('[spl_mailgun_current title="Read the current newsletter &rarr;" class="btn btn-block btn-warning"]'); ?>
     </div>
