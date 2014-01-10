e23db3e9b122be387fa56aa8c47f46eb3a7081c7
Sean Girard <seangirard@yahoo.com>
2014-01-10 14:18:05 -0800

    meta

--- templates/entry-meta.php
+++ templates/entry-meta.php
@@ -1,6 +1,6 @@
 
-<p class="byline author vcard text-muted"><?php echo __('&mdash', 'roots'); ?> 
+<p class="byline author vcard text-muted"><?php echo __('&mdash;', 'roots'); ?> 
 	<a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author" class="fn"><?php echo get_the_author(); ?></a>
-	<time class="published pull-right" datetime="<?php echo get_the_time('c'); ?>">Published <?php echo get_the_date(); ?></time>
+	<time class="published pull-right" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
 </p>
 
