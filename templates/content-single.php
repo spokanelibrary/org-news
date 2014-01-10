777256828ced997b9e626b670a34c050f85a13fb
Sean Girard <seangirard@yahoo.com>
2014-01-10 14:11:50 -0800

    meata

--- templates/content-single.php
+++ templates/content-single.php
@@ -2,11 +2,19 @@
   <article <?php post_class(); ?>>
     <header class="page-header">
       <h1 class="entry-title"><?php the_title(); ?></h1>
-      <?php get_template_part('templates/entry-meta'); ?>
+      <?php //get_template_part('templates/entry-meta'); ?>
     </header>
     <div class="entry-content">
       <?php the_content(); ?>
     </div>
+
+    <?php get_template_part('templates/entry-meta'); ?>
+    <p class="lead text-center text-muted">
+      &hellip;
+      <i class="glyphicon glyphicon-leaf" style="padding: 0 8px 0 12px;"></i>
+      &hellip;
+    </p>
+
     <footer>
       <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
     </footer>
