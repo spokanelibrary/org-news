46d22c45c84850285d2f2f2857de0b99692934ad
Sean Girard <seangirard@yahoo.com>
2014-01-10 08:05:48 -0800

    home

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -7,7 +7,7 @@
 
   <?php echo do_shortcode('[spl_mailgun_current format=toc link_posts=true]'); ?>
   <hr>
-  <?php echo do_shortcode('[spl_mailgun_current title="Read the current newsletter &rarr;" class="btn btn-block btn-warning"]'); ?>
+  <?php echo do_shortcode('[spl_mailgun_current title="Read the current issue &rarr;" class="btn btn-block btn-warning"]'); ?>
 
 </div>
 
