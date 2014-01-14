80a518d2881fa5fc0a70eb3b6a8a89cae8843c9a
Sean Girard <seangirard@yahoo.com>
2014-01-14 10:14:25 -0800

    small headlines

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '3bb530a42a209937d47ce9eef4cf43f9');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '3d7e7ab21211097d8ee46e582b61f6d3');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
